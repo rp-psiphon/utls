183ce2cd2407e4b86eded5a7b46dc63aa1176671
rp-psiphon <r.peccia@psiphon.ca>
2022-06-21 10:18:45 -0400

    added new clientHellos to example

--- handshake_example/main.go
+++ handshake_example/main.go
@@ -22,16 +22,31 @@ var requestHostname = "cloudflare.com" // speaks http2 and TLS 1.3
 var requestAddr = "cloudflare.com:443"
 
 func main() {
-	var err error = TlsHandshake(requestHostname, requestAddr)
-	if err != nil {
-		fmt.Printf("#> TlsHandshake() failed: %+v\n", err)
-	} else {
-		fmt.Println("success")
+	//hellos to test:
+	/*
+		HelloChrome_87
+		HelloChrome_96
+		HelloIOS_13
+		HelloIOS_14
+		HelloAndroid_11_OkHttp
+
+	*/
+	helloList := []tls.ClientHelloID{tls.HelloChrome_87, tls.HelloChrome_96, tls.HelloIOS_13, tls.HelloIOS_13, tls.HelloIOS_14, tls.HelloAndroid_11_OkHttp}
+
+	for _, helloType := range helloList {
+
+		var err error = TlsHandshake(requestHostname, requestAddr, helloType)
+		if err != nil {
+			fmt.Printf("#> TlsHandshake() failed: %+v\n", err)
+		} else {
+			fmt.Println("success!")
+		}
+
 	}
 
 }
 
-func TlsHandshake(hostname string, addr string) error {
+func TlsHandshake(hostname string, addr string, helloType tls.ClientHelloID) error {
 	config := tls.Config{ServerName: hostname}
 	dialConn, err := net.DialTimeout("tcp", addr, dialTimeout)
 	if err != nil {
@@ -40,7 +55,7 @@ func TlsHandshake(hostname string, addr string) error {
 	uTlsConn := tls.UClient(dialConn, &config, tls.HelloCustom)
 	defer uTlsConn.Close()
 
-	spec := GenerateSpec("hi")
+	spec := GenerateSpec(helloType)
 	err = uTlsConn.ApplyPreset(&spec)
 
 	if err != nil {
@@ -55,53 +70,9 @@ func TlsHandshake(hostname string, addr string) error {
 	return err
 }
 
-func GenerateSpec(parameter string) tls.ClientHelloSpec {
+func GenerateSpec(id tls.ClientHelloID) tls.ClientHelloSpec {
 
-	spec := tls.ClientHelloSpec{
-		TLSVersMax: tls.VersionTLS13,
-		TLSVersMin: tls.VersionTLS10,
-		CipherSuites: []uint16{
-			tls.GREASE_PLACEHOLDER,
-			tls.TLS_ECDHE_ECDSA_WITH_CHACHA20_POLY1305,
-			tls.TLS_ECDHE_ECDSA_WITH_AES_128_GCM_SHA256,
-			tls.TLS_ECDHE_ECDSA_WITH_AES_256_CBC_SHA,
-			tls.TLS_ECDHE_RSA_WITH_AES_128_CBC_SHA,
-			tls.TLS_AES_128_GCM_SHA256, // tls 1.3
-			tls.FAKE_TLS_DHE_RSA_WITH_AES_256_CBC_SHA,
-			tls.TLS_ECDHE_RSA_WITH_AES_128_GCM_SHA256,
-			tls.TLS_RSA_WITH_AES_256_CBC_SHA,
-		},
-		Extensions: []tls.TLSExtension{
-			&tls.SNIExtension{},
-			&tls.SupportedCurvesExtension{Curves: []tls.CurveID{tls.X25519, tls.CurveP256}},
-			&tls.SupportedPointsExtension{SupportedPoints: []byte{0}}, // uncompressed
-			&tls.SessionTicketExtension{},
-			&tls.ALPNExtension{AlpnProtocols: []string{"myFancyProtocol", "http/1.1"}},
-			&tls.SignatureAlgorithmsExtension{SupportedSignatureAlgorithms: []tls.SignatureScheme{
-				tls.ECDSAWithP256AndSHA256,
-				tls.ECDSAWithP384AndSHA384,
-				tls.ECDSAWithP521AndSHA512,
-				tls.PSSWithSHA256,
-				tls.PSSWithSHA384,
-				tls.PSSWithSHA512,
-				tls.PKCS1WithSHA256,
-				tls.PKCS1WithSHA384,
-				tls.PKCS1WithSHA512,
-				tls.ECDSAWithSHA1,
-				tls.PKCS1WithSHA1}},
-			&tls.KeyShareExtension{[]tls.KeyShare{
-				{Group: tls.CurveID(tls.GREASE_PLACEHOLDER), Data: []byte{0}},
-				{Group: tls.X25519},
-			}},
-			&tls.PSKKeyExchangeModesExtension{[]uint8{1}}, // pskModeDHE
-			&tls.SupportedVersionsExtension{[]uint16{
-				tls.VersionTLS13,
-				tls.VersionTLS12,
-				tls.VersionTLS11,
-				tls.VersionTLS10}},
-		},
-		GetSessionID: nil,
-	}
+	spec := tls.utlsIDToSpec(tls.clientHelloID)
 
 	return spec
 
